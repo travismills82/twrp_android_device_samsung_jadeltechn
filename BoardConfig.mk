78e2c38b7e6d6184cd19c250d65a38832d546798
travis mills <travismills82@gmail.com>
2018-04-04 02:35:45 +0800

    add brightness path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS:= true
 
 # TWRP
-#TW_BRIGHTNESS_PATH := "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/backlight/panel0-backlight/brightness"
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 #TARGET_USE_CUSTOM_LUN_FILE_PATH := "sys/class/android_usb/android0/f_mass_storage/lun/file"
 TW_INCLUDE_NTFS_3G := true 
 TW_NO_EXFAT_FUSE := true
